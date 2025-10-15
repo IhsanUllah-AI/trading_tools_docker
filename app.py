df0e512018bbd3e158c2dd07e05b65f365932531
Ihsan Ullah <148381681+IhsanUllah-AI@users.noreply.github.com>
2025-10-15 16:22:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, session, jsonify
 from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
-app.secret_key = 'c74b83096eb1d42b804378494fe15bfc'
+app.secret_key = 'your_secret_key_here'
 
 scheduler = APScheduler()
 scheduler.init_app(app)
